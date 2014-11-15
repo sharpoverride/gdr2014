484159dc653d3a2d547ed9a79341108b45ffcd05
Mihai Lazar <mihai.lazar@evozon.com>
2014-11-15 15:53:18 +0200

    First test

---
+++ GameOfLife/GameOfLife/GameOfLifeTets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife
+{
+    [TestClass]
+    public class GameOfLifeTests
+    {
+        [TestMethod]
+        public void Run_OneCellIsAlive_WhenRegistered()
+        {
+            var game = new GameOfLife();
+
+            game.Register(new Cell(1, 1));
+
+            Assert.IsTrue(game.isAliveAt(1,1));
+        }
+    }
+
+    public class GameOfLife
+    {
+        public GameOfLife()
+        {
+
+        }
+
+        public void Register(Cell cell)
+        {
+        }
+
+        public bool isAliveAt(int x, int y)
+        {
+            return true;
+        }
+    }
+
+    public class Cell
+    {
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int Y { get; set; }
+
+        public int X { get; set; }
+    }
+}
