8a2dfa3812bae8dfedf4c1c3178fc9be006aec21
Mihai Lazar <mihai.lazar@evozon.com>
2014-11-15 17:40:45 +0200

    finished

--- GameOfLife/GameOfLife/GameOfLifeTets.cs
+++ GameOfLife/GameOfLife/GameOfLifeTets.cs
@@ -15,7 +15,7 @@ namespace GameOfLife
 
             game.Register(new Cell(1, 1));
 
-            Assert.IsTrue(game.isAliveAt(1,1));
+            Assert.IsTrue(game.isAliveAt(1, 1));
         }
 
         [TestMethod]
@@ -27,7 +27,7 @@ namespace GameOfLife
         }
 
         [TestMethod]
-        public void Run_KillsCell_WithNoNeighbors()
+        public void Run_WithSingleCell_CellIsDead()
         {
             var game = new GameOfLife();
 
@@ -39,7 +39,21 @@ namespace GameOfLife
         }
 
         [TestMethod]
-        public void Run_KeepsCells_WithTwoNeighbors()
+        public void Run_CellWithOnlyOneNeighbour_CellIsDead()
+        {
+            var game = new GameOfLife();
+
+            game.Register(new Cell(0, 1));
+            game.Register(new Cell(1, 1));
+            game.Register(new Cell(2, 5));
+
+            game.Run();
+
+            Assert.IsFalse(game.isAliveAt(1, 1));
+        }
+
+        [TestMethod]
+        public void Run_CellWithTwoNeighbors_CellIsAlive()
         {
             var game = new GameOfLife();
 
@@ -53,7 +67,7 @@ namespace GameOfLife
         }
 
         [TestMethod]
-        public void Run_KeepsCells_WithThreeNeighbors()
+        public void Run_CellWithThreeNeighbors_CellIsAlive()
         {
             var game = new GameOfLife();
 
@@ -68,7 +82,7 @@ namespace GameOfLife
         }
 
         [TestMethod]
-        public void Run_KillsCells_WithFourNeighbors()
+        public void Run_CellWithFourNeighbors_CellIsDead()
         {
             var game = new GameOfLife();
 
@@ -83,15 +97,38 @@ namespace GameOfLife
             Assert.IsFalse(game.isAliveAt(1, 1));
         }
 
+        [TestMethod]
+        public void Run_DeadCell11WithExactlyThreeNeighbors_CellIsResurected()
+        {
+            var game = new GameOfLife();
+
+            game.Register(new Cell(0, 1));
+            game.Register(new Cell(2, 1));
+            game.Register(new Cell(2, 0));
+
+            game.Run();
+
+            Assert.IsTrue(game.isAliveAt(1, 1));
+        }
+
+        [TestMethod]
+        public void Run_DeadCell22WithExactlyThreeNeighbors_CellIsResurected()
+        {
+            var game = new GameOfLife();
+
+            game.Register(new Cell(1, 2));
+            game.Register(new Cell(3, 2));
+            game.Register(new Cell(3, 1));
+
+            game.Run();
+
+            Assert.IsTrue(game.isAliveAt(2, 2));
+        }
     }
 
     public class GameOfLife
     {
         List<Cell> _cells = new List<Cell>();
-        public GameOfLife()
-        {
-
-        }
 
         public void Register(Cell cell)
         {
@@ -100,27 +137,65 @@ namespace GameOfLife
 
         public bool isAliveAt(int x, int y)
         {
-            return _cells.Contains(new Cell(x,y));
+            return _cells.Contains(new Cell(x, y));
         }
 
         public void Run()
         {
-            var survivingCells = _cells.Where(CanSurvive).ToList();
+            var resurectedCells = GenerateDeadNeighbours().Where(CanBeResurected);
+            var survivingCells = _cells.Where(CanSurvive);
 
-            _cells = survivingCells;
+            _cells = survivingCells.Union(resurectedCells).ToList();
+        }
+
+        private IEnumerable<Cell> GenerateDeadNeighbours()
+        {
+            return _cells.SelectMany(GetCellDeadNeighbours);
+        }
+
+        private IEnumerable<Cell> GetCellDeadNeighbours(Cell cell)
+        {
+            for (int i = cell.X - 1; i < cell.X + 1; i++)
+            {
+                for (int j = cell.Y - 1; j < cell.Y + 1; j++)
+                {
+                    if (isAliveAt(i, j) == false)
+                    {
+                        yield return new Cell(i, j); 
+                    }
+                }                
+            }
         }
 
         private bool CanSurvive(Cell arg)
+        {
+            var count = GetAliveNeighbourCount(arg);
+
+            return count == 2 || count == 3;
+        }
+
+        private bool CanBeResurected(Cell arg)
+        {
+            var count = GetAliveNeighbourCount(arg);
+
+            return count == 3;
+        }
+
+        private int GetAliveNeighbourCount(Cell arg)
         {
             var count = _cells.Count(cell =>
             {
                 var dx = Math.Abs(cell.X - arg.X);
                 var dy = Math.Abs(cell.Y - arg.Y);
 
-                return (dx <= 1 || dy <= 1) && (dx + dy != 0);
+                return (dx <= 1 && dy <= 1) && !IsSameCell(dx, dy);
             });
-            
-            return count == 2 || count == 3;
+            return count;
+        }
+
+        private bool IsSameCell(int dx, int dy)
+        {
+            return (dx == 0 && dy == 0);
         }
     }
 
